6ea12059dcc4ad91b31d310ec8acd41cf33bc00d
Robert Hitchcock <robert.e.hitchcock@gmail.com>
2023-08-11 15:59:00 -0400

    reconnect

--- client/firebase.js
+++ client/firebase.js
@@ -3,11 +3,11 @@ import firebase from "firebase"
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDFm1b1yGGkls4e_CVuLtQ19s2KbIomMcg",
-    authDomain: "amzon-2-51286.firebaseapp.com",
-    projectId: "amzon-2-51286",
-    storageBucket: "amzon-2-51286.appspot.com",
-    messagingSenderId: "759549730660",
-    appId: "1:759549730660:web:05b7034ff21f35b03be471",
-    measurementId: "G-HK5K91KEH0"
-  };
+  apiKey: "AIzaSyADLtvZC46xMjnBe2si52wVST5JXHFLWNQ",
+  authDomain: "amzon-2-51286.firebaseapp.com",
+  projectId: "amzon-2-51286",
+  storageBucket: "amzon-2-51286.appspot.com",
+  messagingSenderId: "759549730660",
+  appId: "1:759549730660:web:05b7034ff21f35b03be471",
+  measurementId: "G-HK5K91KEH0"
+};
